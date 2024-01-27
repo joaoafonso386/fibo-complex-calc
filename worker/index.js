594a0571e2a91a6913966d93cc3c4bbfc82bd15a
João Afonso <joaoafonso386@gmail.com>
2024-01-27 19:51:08 +0000

    Abstract redis logic to common

--- worker/index.js
+++ worker/index.js
@@ -1,29 +1,16 @@
 import { keys } from "./keys.js";
-import { createClient } from "redis";
 import { fibo } from "./fibo.js";
+import { initRedis } from "../common/redis.mjs"
+import { createClient } from "redis"
 
-let client;
+const { client } = await initRedis(keys, createClient)
+const sub = client.duplicate();
 
-try {
-  client = await createClient({
-    legacyMode: true,
-    socket: {
-      host: keys.redisHost,
-      reconnectStrategy: () => 1000,
-    },
-  }).connect();
-  console.log("Connected to Redis!");
+sub.on("message", (_, message) => {
+  client.hset("values", message, fibo(parseInt(message)));
+});
 
-  const sub = client.duplicate();
-
-  sub.on("message", (_, message) => {
-    client.hset("values", message, fibo(parseInt(message)));
-  });
-
-  sub.subscribe("insert");
-} catch (e) {
-  console.error("Redis Client Error", e);
-}
+sub.subscribe("insert");
 
 process.on("SIGTERM", async () => {
   await client.disconnect();
