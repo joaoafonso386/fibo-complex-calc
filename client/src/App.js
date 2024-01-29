8d42bbfc9a9c2bf3eec4bcbba9ae12e2b7cc0e35
João Afonso <joaoafonso386@gmail.com>
2024-01-29 00:39:32 +0000

    Init React client

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState } from 'react'
+
+const App = () => {
+
+  const [values, setValues] = useState([])
+  const [index, setIndex] = useState([])
+  const [seenIndexes, setSeenIndexes] = useState([])
+
+
+  return (
+    <div className="App">
+      Hi!
+    </div>
+  );
+}
+
+export default App;
